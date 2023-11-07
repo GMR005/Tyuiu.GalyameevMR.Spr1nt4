da6c85a07ec216f0cfecb0bf76f6468c0a9e235b
GMR005 <mihailgalemeev@mail.ru>
2023-11-07 17:45:08 +0500

    Task4

---
+++ Tyuiu.GalyameevMR.Sprint4.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GalyameevMR.Sprint4.Task4.V8.Lib;
+
+namespace Tyuiu.GalyameevMR.Sprint4.Task4.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChangeIndexInMatrix()
+        {
+            DataService DataService = new DataService();
+            int [,] matrix = { { 1, 3, 2, 2, 5 }, { 6, 1, 5, 5, 2 }, { 1, 5, 4, 1, 4 }, { 4, 1, 4, 4, 5 }, { 4, 6, 1, 1, 4 } };
+            int[,] waitMatrix = { { 1, 3, 1, 1, 5 }, { 1, 1, 5, 5, 1}, { 1, 5, 1, 1, 1 }, { 1, 1, 1, 1, 5 }, { 1, 1, 1, 1, 1 } };
+            int[,] wait = DataService.Calculate(matrix);
+            CollectionAssert.AreEqual(waitMatrix, wait);
+        }
+    }
+}
---
+++ Tyuiu.GalyameevMR.Sprint4.Task4.V8/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GalyameevMR.Sprint4.Task4.V8.Lib;
+
+namespace Tyuiu.GalyameevMR.Sprint4.Task4.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService DataService = new DataService();
+            Console.Title = "Спринт 4. Выполнил: Галямеев М.Р. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("* Спринт #4                                                     ");
+            Console.WriteLine("* Тема: Двумерные массивы                                       ");
+            Console.WriteLine("* Задание #4                                                    ");
+            Console.WriteLine("* Вариант #8                                                   ");
+            Console.WriteLine("* Выполнил: Галямеев М.Р. | ПКТб-23-1                           ");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                      ");
+            Console.WriteLine("Дан двумерный целочисленный массив 5 на 5 элементов, заполненный" +
+                " значениями с клавиатуры в диапазоне от 1 до 6. Заменить четные элементы массива на 1.");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                              *");
+            Console.WriteLine();
+            int[,] mtrx = new int[5, 5];
+            for (int i = 0; i < mtrx.GetLength(0); i++)
+            {
+                for (int j = 0; j < mtrx.GetLength(1); j++)
+                {
+                    Console.Write($"Введите {i}, {j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Матрица: ");
+            for (int i = 0; i < mtrx.GetLength(0); i++)
+            {
+                        for (int j = 0; j < mtrx.GetLength(1); j++)
+             {
+                            Console.Write(mtrx[i, j] + "\t");
+                        }
+                        Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                     ");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine();
+            int[,] result = DataService.Calculate(mtrx);
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write(result[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
+
